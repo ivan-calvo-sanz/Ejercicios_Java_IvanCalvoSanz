27c984513f645638eb50ea55157bfc598f89839a
ivan-calvo-sanz <ivan.calvo.sanz@gmail.com>
2025-12-26 20:09:37 +0100

    251226

---
+++ src/_2_POO_ejercicio_06_Empleado/Empresa.java
@@ -0,0 +1,37 @@
+package _2_POO_ejercicio_06_Empleado;
+
+public class Empresa {
+
+    private String CIF;
+    private String nombre;
+
+    public Empresa() {
+    }
+
+    public Empresa(String CIF, String nombre) {
+        this.CIF = CIF;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "CIF=" + CIF + ", nombre=" + nombre + '}';
+    }
+
+    public String getCIF() {
+        return CIF;
+    }
+
+    public void setCIF(String CIF) {
+        this.CIF = CIF;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
