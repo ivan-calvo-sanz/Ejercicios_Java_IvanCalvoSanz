1dc5d4588af1b157c2f7f7fb3668ccfcc138c27f
ivan-calvo-sanz <ivan.calvo.sanz@gmail.com>
2025-12-27 19:48:42 +0100

    251226

---
+++ src/_2_POO_ejercicio_07_Producto/ProductoRefigerado.java
@@ -0,0 +1,29 @@
+package _2_POO_ejercicio_07_Producto;
+
+public class ProductoRefigerado extends Producto {
+
+    private int cajon;
+
+    public ProductoRefigerado() throws IllegalArgumentException {
+        this("", 0, 0);
+    }
+
+    public ProductoRefigerado(String nombre, double precio, int cajon) throws IllegalArgumentException {
+        super(nombre, precio);
+        this.cajon = cajon;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " ProductoRefigerado{" + "cajon=" + cajon + '}';
+    }
+
+    public int getCajon() {
+        return cajon;
+    }
+
+    public void setCajon(int cajon) {
+        this.cajon = cajon;
+    }
+
+}
